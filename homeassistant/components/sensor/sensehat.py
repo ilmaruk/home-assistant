902abb6b65a34fc074a25bd45c1ef45066c295c9
ilmaruk <ruggero.ferretti@gmail.com>
2018-03-05 22:52:41 +0000

    One class per sensor type.

--- homeassistant/components/sensor/sensehat.py
+++ homeassistant/components/sensor/sensehat.py
@@ -8,7 +8,6 @@ import os
 import logging
 from datetime import timedelta
 
-import typing
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -23,9 +22,11 @@ _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = 'sensehat'
 CONF_IS_HAT_ATTACHED = 'is_hat_attached'
+CONF_TEMPERATURE_FROM_SENSOR = 'temperature_from_sensor'
 
+HUMIDITY_SENSOR = 'humidity'
+PRESSURE_SENSOR = 'pressure'
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
-
 MAX_READING_HISTORY_SIZE = 5
 
 SENSOR_TYPES = {
@@ -35,46 +36,130 @@ SENSOR_TYPES = {
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_DISPLAY_OPTIONS, default=list(SENSOR_TYPES)):
+    vol.Required(CONF_DISPLAY_OPTIONS, default=SENSOR_TYPES.keys()):
         [vol.In(SENSOR_TYPES)],
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_IS_HAT_ATTACHED, default=True): cv.boolean
+    vol.Optional(CONF_IS_HAT_ATTACHED, default=True): cv.boolean,
+    vol.Optional(CONF_TEMPERATURE_FROM_SENSOR, default='both'): cv.string,
 })
 
 
-def setup_platform(_, config, add_devices, __=None) -> None:
+class GenericSensor(Entity):
+
+    def __init__(self, sense_hat, name, unit_of_measurement):
+        self._sense_hat = sense_hat
+        self._name = name
+        self._unit_of_measurement = unit_of_measurement
+        self._state = None
+
+    def update(self):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return self._unit_of_measurement
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+
+class HumiditySensor(GenericSensor):
+
+    def __init__(self, sense_hat):
+        super().__init__(sense_hat, 'humidity', '%')
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        self._state = self._sense_hat.get_humidity()
+
+
+class PressureSensor(GenericSensor):
+
+    def __init__(self, sense_hat):
+        super().__init__(sense_hat, 'pressure', 'mb')
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        self._state = self._sense_hat.get_pressure()
+
+
+class TemperatureSensor(GenericSensor):
+
+    def __init__(self, sense_hat, from_sensor, is_attached):
+        super().__init__(sense_hat, 'temperature', TEMP_CELSIUS)
+        self._from_sensor = from_sensor
+        self._is_attached = is_attached
+        self._readings_history = []
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        temperature = self._get_temperature()
+
+        # If the SenseHAT is attached, we need to consider the CPU temperature
+        temperature = _normalise_current_temperature(temperature,
+                                                     self._is_attached)
+
+        # Keep the reading history up to date with the raw values
+        _update_readings_history(self._readings_history, temperature,
+                                 MAX_READING_HISTORY_SIZE)
+
+        # Return the average of the reading history
+        self._state = sum(self._readings_history) / len(self._readings_history)
+
+    def _get_temperature(self):
+        if self._from_sensor == 'humidity':
+            temperature = self._sense_hat.get_temperature_from_humidity()
+        elif self._from_sensor == 'pressure':
+            temperature = self._sense_hat.get_temperature_from_pressure()
+        else:
+            temperature = (self._sense_hat.get_temperature_from_humidity() +
+                           self._sense_hat.get_temperature_from_pressure()) / 2
+
+        return temperature
+
+
+def _sensor_factory(config, sensor_type, sense_hat):
+    """Instantiate an instance of the required sensor class."""
+    if sensor_type == 'humidity':
+        return HumiditySensor(sense_hat)
+    elif sensor_type == 'pressure':
+        return PressureSensor(sense_hat)
+    elif sensor_type == 'temperature':
+        return TemperatureSensor(sense_hat,
+                                 config[CONF_TEMPERATURE_FROM_SENSOR],
+                                 config[CONF_IS_HAT_ATTACHED])
+    else:
+        return None
+
+
+def setup_platform(_, config, add_devices, __=None):
     """Set up the Sense HAT sensor platform."""
     from sense_hat import SenseHat
-    dev = []
+    devices = []
     for sensor_type in config[CONF_DISPLAY_OPTIONS]:
-        dev.append(SenseHatSensor(
-            SenseHat(), CONF_IS_HAT_ATTACHED.lower() == 'true', sensor_type))
+        sensor = _sensor_factory(config, sensor_type, SenseHat())
+        if sensor is not None:
+            devices.append(sensor)
 
-    add_devices(dev, True)
-
-
-def _get_temperature_average(
-        readings: typing.List[typing.Optional[float]]) -> typing.Optional[float]:
-    """Given a list of temperature readings, work out the
-    average temperature, excluding None's.
-    """
-    valid_readings: typing.List[float] = [r for r in readings if r is not None]
-    if len(valid_readings) == 0:
-        return None
-    return sum(valid_readings) / len(valid_readings)
+    add_devices(devices, True)
 
 
-def _get_cpu_temperature() -> float:
+def _get_cpu_temperature():
     """Get the CPU temperature."""
     cpu_temperature_info = os.popen("vcgencmd measure_temp").readline()
     print(cpu_temperature_info)
     return float(cpu_temperature_info.replace("temp=", "").replace("'C\n", ""))
 
 
-def _normalise_current_temperature(
-        temperature: typing.Optional[float],
-        hat_attached: bool
-) -> typing.Optional[float]:
+def _normalise_current_temperature(temperature, hat_attached):
     """If the SenseHAT is attached to the Pi, its temperature is
     affected by the CPU temperature and therefore we need to normalise
     the physical sensor readings.
@@ -86,80 +171,9 @@ def _normalise_current_temperature(
     return temperature - ((cpu_temperature - temperature) / 1.5)
 
 
-def _update_readings_history(
-        readings_history: typing.List[typing.Optional[float]],
-        new_reading: typing.Optional[float],
-        max_history_size: int=MAX_READING_HISTORY_SIZE
-) -> None:
+def _update_readings_history(readings_history, new_reading, max_history_size):
     """Make sure a readings history doesn't overflow."""
     readings_history.append(new_reading)
     if len(readings_history) > max_history_size:
         for _ in range(len(readings_history) - max_history_size):
             readings_history.pop(0)
-
-
-class SenseHatSensor(Entity):
-    """Representation of a SenseHAT sensor."""
-
-    def __init__(self, sense_hat: 'SenseHat', hat_attached: bool,
-                 sensor_type: str) -> None:
-        """Initialize the sensor."""
-        self._sense_hat = sense_hat
-        self._hat_attached = hat_attached
-        self._type = sensor_type
-        self._name = SENSOR_TYPES[sensor_type][0]
-        self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
-        self._state = None
-        self._readings_history = list()
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self) -> typing.Optional[float]:
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self) -> str:
-        """Return the unit the value is expressed in."""
-        return self._unit_of_measurement
-
-    @property
-    def humidity(self) -> typing.Optional[float]:
-        """Humidity getter."""
-        return self._sense_hat.get_humidity()
-
-    @property
-    def pressure(self) -> typing.Optional[float]:
-        """Pressure getter."""
-        return self._sense_hat.get_pressure()
-
-    @property
-    def temperature(self) -> typing.Optional[float]:
-        """Temperature getter."""
-        # Use all the sensors that can report temperature
-        temperature = _get_temperature_average(
-            [self._sense_hat.get_temperature_from_humidity(),
-             self._sense_hat.get_temperature_from_pressure()])
-
-        # If the SenseHAT is attached, we need to consider the CPU temperature
-        temperature = _normalise_current_temperature(
-            temperature, self._hat_attached)
-
-        # Keep the reading history up to date with the raw values
-        _update_readings_history(self._readings_history, temperature)
-
-        # Return the average of the reading history
-        return _get_temperature_average(self._readings_history)
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-        """Update the sensor state."""
-        self._state = getattr(self, self._type)
-
-        if self._state is None:
-            _LOGGER.warning(
-                "Unable to update {} sensor reading.".format(self._type))
--- tests/components/sensor/test_sensehat.py
+++ tests/components/sensor/test_sensehat.py
@@ -1,19 +1,14 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
-from homeassistant.components.sensor.sensehat import SenseHatSensor,\
-    _get_temperature_average, _normalise_current_temperature,\
-    _get_cpu_temperature, _update_readings_history
+from homeassistant.components.sensor.sensehat import \
+    _normalise_current_temperature, \
+    _get_cpu_temperature, _update_readings_history, HumiditySensor, \
+    PressureSensor, TemperatureSensor
+from homeassistant.const import TEMP_CELSIUS
 
 
 class TestSenseHat(unittest.TestCase):
-
-    def test_get_current_temperature_average(self) -> None:
-        self.assertEqual(
-            _get_temperature_average([20, None, 21.2, None, 22.4]), 21.2)
-
-    def test_get_current_temperature_average_all_none(self) -> None:
-        self.assertIsNone(_get_temperature_average([None, None, None]))
 
     @patch('os.popen')
     def test_get_cpu_temperature(self, mock_os_popen: MagicMock) -> None:
@@ -51,72 +46,143 @@ class TestSenseHat(unittest.TestCase):
         self.assertEqual(history, [12, 13, 14])
 
 
-class TestSenseHatSensor(unittest.TestCase):
-    """Test the SenseHAT sensor."""
+class TestHumiditySensor(unittest.TestCase):
 
     def setUp(self):
         super().setUp()
         self._sense_hat = MagicMock()
+        self._sut = HumiditySensor(self._sense_hat)
 
-    def test_update_humidity(self):
-        self._test_update('humidity', 56.7)
+    def test_name(self):
+        self.assertEqual(self._sut.name, 'humidity')
 
-    def test_failed_update_humidity(self):
-        self._test_update('humidity', None)
+    def test_unit_of_measurement(self):
+        self.assertEqual(self._sut.unit_of_measurement, '%')
 
-    def test_update_pressure(self):
-        self._test_update('pressure', 1234.5)
+    def test_update(self):
+        self._sense_hat.get_humidity.return_value = 56.789
+        self._sut.update()
+        self.assertEqual(self._sut.state, 56.789)
 
-    def test_failed_update_pressure(self):
-        self._test_update('pressure', None)
 
-    def test_update_temperature(self):
-        self._sense_hat.get_temperature_from_humidity.return_value = 19.5
-        self._sense_hat.get_temperature_from_pressure.return_value = 21.5
-        sut = self._make_sut('temperature')
+class TestPressureSensor(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self._sense_hat = MagicMock()
+        self._sut = PressureSensor(self._sense_hat)
+
+    def test_name(self):
+        self.assertEqual(self._sut.name, 'pressure')
+
+    def test_unit_of_measurement(self):
+        self.assertEqual(self._sut.unit_of_measurement, 'mb')
+
+    def test_update(self):
+        self._sense_hat.get_pressure.return_value = 987.654
+        self._sut.update()
+        self.assertEqual(self._sut.state, 987.654)
+
+
+class TestTemeratureSensor(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self._sense_hat = MagicMock()
+        self._sut = TemperatureSensor(self._sense_hat, 'both', False)
+
+    def test_name(self):
+        self.assertEqual(self._sut.name, 'temperature')
+
+    def test_unit_of_measurement(self):
+        self.assertEqual(self._sut.unit_of_measurement, TEMP_CELSIUS)
+
+    def test_update_from_both(self):
+        self._sense_hat.get_temperature_from_humidity.return_value = 20.5
+        self._sense_hat.get_temperature_from_pressure.return_value = 22.5
+        self._sut.update()
+        self.assertEqual(self._sut.state, 21.5)
+
+    def test_update_from_humidity(self):
+        self._sense_hat.get_temperature_from_humidity.return_value = 20.5
+        self._sense_hat.get_temperature_from_pressure.return_value = 22.5
+        sut = TemperatureSensor(self._sense_hat, 'humidity', False)
         sut.update()
         self.assertEqual(sut.state, 20.5)
 
-    def test_update_temperature_only_from_humidity(self):
-        self._sense_hat.get_temperature_from_humidity.return_value = 19.5
-        self._sense_hat.get_temperature_from_pressure.return_value = None
-        sut = self._make_sut('temperature')
+    def test_update_from_pressure(self):
+        self._sense_hat.get_temperature_from_humidity.return_value = 20.5
+        self._sense_hat.get_temperature_from_pressure.return_value = 22.5
+        sut = TemperatureSensor(self._sense_hat, 'pressure', False)
         sut.update()
-        self.assertEqual(sut.state, 19.5)
+        self.assertEqual(sut.state, 22.5)
 
-    def test_update_temperature_only_from_pressure(self):
-        self._sense_hat.get_temperature_from_humidity.return_value = None
-        self._sense_hat.get_temperature_from_pressure.return_value = 21.5
-        sut = self._make_sut('temperature')
-        sut.update()
-        self.assertEqual(sut.state, 21.5)
-
-    def test_update_temperature_no_readings_no_history(self):
-        self._sense_hat.get_temperature_from_humidity.return_value = None
-        self._sense_hat.get_temperature_from_pressure.return_value = None
-        sut = self._make_sut('temperature')
-        sut.update()
-        self.assertIsNone(sut.state)
-
-    def test_update_temperature_no_readings_with_history(self):
-        sut = self._make_sut('temperature')
-        self._sense_hat.get_temperature_from_humidity.return_value = 20
-        self._sense_hat.get_temperature_from_pressure.return_value = 21
-        sut.update()
-        self.assertEqual(sut.state, 20.5)
-        self._sense_hat.get_temperature_from_humidity.return_value = None
-        self._sense_hat.get_temperature_from_pressure.return_value = None
-        sut.update()
-        self.assertEqual(sut.state, 20.5)
-
-    def _test_update(self, sensor_type, reading_value):
-        getattr(self._sense_hat, "get_{}".format(sensor_type))\
-            .return_value = reading_value
-        sut = self._make_sut(sensor_type)
-        sut.update()
-        self.assertEqual(sut.state, reading_value)
-
-    def _make_sut(self, sensor_type: str) -> SenseHatSensor:
-        # hat_attached=False so that temperature is not affected by the CPU
-        return SenseHatSensor(
-            self._sense_hat, hat_attached=False, sensor_type=sensor_type)
+# class TestSenseHatSensor(unittest.TestCase):
+#     """Test the SenseHAT sensor."""
+#
+#     def setUp(self):
+#         super().setUp()
+#         self._sense_hat = MagicMock()
+#
+#     def test_update_humidity(self):
+#         self._test_update('humidity', 56.7)
+#
+#     def test_failed_update_humidity(self):
+#         self._test_update('humidity', None)
+#
+#     def test_update_pressure(self):
+#         self._test_update('pressure', 1234.5)
+#
+#     def test_failed_update_pressure(self):
+#         self._test_update('pressure', None)
+#
+#     def test_update_temperature(self):
+#         self._sense_hat.get_temperature_from_humidity.return_value = 19.5
+#         self._sense_hat.get_temperature_from_pressure.return_value = 21.5
+#         sut = self._make_sut('temperature')
+#         sut.update()
+#         self.assertEqual(sut.state, 20.5)
+#
+#     def test_update_temperature_only_from_humidity(self):
+#         self._sense_hat.get_temperature_from_humidity.return_value = 19.5
+#         self._sense_hat.get_temperature_from_pressure.return_value = None
+#         sut = self._make_sut('temperature')
+#         sut.update()
+#         self.assertEqual(sut.state, 19.5)
+#
+#     def test_update_temperature_only_from_pressure(self):
+#         self._sense_hat.get_temperature_from_humidity.return_value = None
+#         self._sense_hat.get_temperature_from_pressure.return_value = 21.5
+#         sut = self._make_sut('temperature')
+#         sut.update()
+#         self.assertEqual(sut.state, 21.5)
+#
+#     def test_update_temperature_no_readings_no_history(self):
+#         self._sense_hat.get_temperature_from_humidity.return_value = None
+#         self._sense_hat.get_temperature_from_pressure.return_value = None
+#         sut = self._make_sut('temperature')
+#         sut.update()
+#         self.assertIsNone(sut.state)
+#
+#     def test_update_temperature_no_readings_with_history(self):
+#         sut = self._make_sut('temperature')
+#         self._sense_hat.get_temperature_from_humidity.return_value = 20
+#         self._sense_hat.get_temperature_from_pressure.return_value = 21
+#         sut.update()
+#         self.assertEqual(sut.state, 20.5)
+#         self._sense_hat.get_temperature_from_humidity.return_value = None
+#         self._sense_hat.get_temperature_from_pressure.return_value = None
+#         sut.update()
+#         self.assertEqual(sut.state, 20.5)
+#
+#     def _test_update(self, sensor_type, reading_value):
+#         getattr(self._sense_hat, "get_{}".format(sensor_type))\
+#             .return_value = reading_value
+#         sut = self._make_sut(sensor_type)
+#         sut.update()
+#         self.assertEqual(sut.state, reading_value)
+#
+#     def _make_sut(self, sensor_type: str) -> SenseHatSensor:
+#         # hat_attached=False so that temperature is not affected by the CPU
+#         return SenseHatSensor(
+#             self._sense_hat, hat_attached=False, sensor_type=sensor_type)
